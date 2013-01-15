d8f07d033e41d351920785d119ae7dcfb856f294
Sven <mailsvb@gmail.com>
2013-01-15 16:38:10 +0100

    new version of the syslog deamon

--- syslogd.js
+++ syslogd.js
@@ -1,36 +1,92 @@
-//
-// Config
-//
+/* * * *
+	Config
+ * * * */
+
 var config = {
 	end:"\n",
 	folder:__dirname,
-	port: 1514
+	port: 514
 }
 
 var dgram = require("dgram");
 var fs = require('fs');
 var util = require('util');
+fileStreams = new Array();
 
-//
-// Prepare callbacks
-//
 
-var createOrAppendToFileSync = function(msg, rinfo, formattedIp) {
-  if (typeof formattedIp === 'undefined' || formattedIp === null) {
-    formattedIp = rinfo.address;
-  }
-  // create folder if it does not exist
-	if (!fs.existsSync(config.folder + "\\" + formattedIp)) {
-		fs.mkdirSync(config.folder + "\\" + formattedIp);
+/* * * *
+	binding sockets
+ * * * */
+
+var server4 = dgram.createSocket("udp4");
+var server6 = dgram.createSocket("udp6");
+
+[server4, server6].map(function(socket) {
+	socket.on('listening', function() {
+		var addressInfo = this.address();
+		console.log("server listening " + addressInfo.address + ":" + addressInfo.port);
+	});
+	socket.on('error', socketErrorHandler);
+	socket.on('message', socketMessageHandler);
+	socket.bind(config.port);
+});
+
+
+/* * * *
+	callback functions
+ * * * */
+
+var socketMessageHandler = function(msg, rinfo) {
+	// replace : in IPv6 addresses
+	var ip = rinfo.address.replace(/:/g, ".");
+	
+	// create folder IP based if it does not exist
+	if (!fs.existsSync(config.folder + "\\" + ip)) {
+		fs.mkdirSync(config.folder + "\\" + ip);
 	}
 	
-	var filename = util.format('%s\\%s\\%s_%s.txt', config.folder, formattedIp, formattedIp, formatDate());
+	var now 		= getDate();
+	var filename 	= util.format('%s\\%s\\%s_%s.txt', config.folder, ip, ip, now);
 	
-	fs.appendFileSync(filename, msg + config.end);
+	try {
+		if (!fileStreams[ip]) {
+			fileStreams[ip] = {	date : now, stream: fs.createWriteStream(filename, {'flags' : 'a'})};
+			fileStreams[ip].stream.setMaxListeners(0);
+		}
+		
+		if (fileStreams[ip].date != now) {
+			fileStreams[ip].stream.destroySoon();
+			fileStreams[ip] = {	date : now, stream: fs.createWriteStream(filename, {'flags' : 'a'})};
+			fileStreams[ip].stream.setMaxListeners(0);
+		}
+	}
+	catch(error) {
+		console.error(error);
+	}
+	
+	fileStreams[ip].stream.on('error', function(error) {
+		console.error('error on stream write', error, this);
+	});
+		
+	// writing log message to stream
+	fileStreams[ip].stream.write(msg + config.end);
 };
 
-var formatDate = function(date) {
-  var now = date || (new Date());
+var socketErrorHandler = function(exception) {
+	console.error("A Socketerror occured!");
+	if (config.port < 1024) {
+		console.warn('Your port ist set lower than 1024, maybe you are not privileged to use it.');
+	}
+	console.warn(exception);
+};
+
+
+/* * * *
+	general functions
+ * * * */
+
+var getDate = function(date) {
+	var now = date || (new Date());
 	var YY = now.getFullYear();
 	var MM = (now.getMonth() + 1);
 		if (MM < 10) { MM = '0' + MM; }
@@ -38,37 +94,3 @@ var formatDate = function(date) {
 		if (DD < 10) { DD = '0' + DD; }
 	return YY + "_" + MM + "_" + DD;
 }
-
-var socketListeningHandler = function() {
-  var addressInfo = this.address();
-  console.log("server listening " +
-    addressInfo.address + ":" + addressInfo.port);
-};
-
-var socketErrorHandler = function(exception) {
-  console.error("A Socketerror occured!");
-  if (config.port < 1024) {
-    console.warn('Your port ist set lower than 1024, maybe you are not privileged to use it.');
-  }
-  console.warn(exception);
-};
-
-//
-// Start Sockets
-//
-
-var server4 = dgram.createSocket("udp4");
-var server6 = dgram.createSocket("udp6");
-
-server4.on("message", createOrAppendToFileSync);
-
-server6.on("message", function (msg, rinfo) {
-	var ip = rinfo.address.replace(/:/g, "."); // replace ':' in IPv6 address
-	createOrAppendToFileSync(msg, rinfo, ip);
-});
-
-[server4, server6].map(function(socket) {
-  socket.on('listening', socketListeningHandler);
-  socket.on('error', socketErrorHandler);
-  socket.bind(config.port);
-});
